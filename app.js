abd0df12a158d9b4ffa5ca39f33088dc1a88aa01
sumair-khan21 <johnalex21210@gmail.com>
2022-11-12 16:07:47 +0500

    Work Complete

---
+++ app.js
@@ -0,0 +1,51 @@
+// var city0 = "karachi";
+// var city1 = "lahore"; 
+// var city3 = "islamabad";
+// var city4 = "multan";
+
+// var a = ["karachi", "lahore", "islamabad", "multan"];
+
+// // a[1] = "ABC";
+// // a[3] = "karachi";
+
+// console.log(a);
+ 
+// var fruit1 = "Orange";
+// var fruit2 = "Apple";
+// var fruit3 = "Grapes";
+// var fruit4 = "banana";
+
+// var a = ["Orange", "Apple", "Grapes", "banana"];
+
+// a[1] = "ABC";
+// a[3] = "Apple";
+
+// a.push("banana");
+// a.pop();
+// a.shift();
+// a.unshift("ABC");
+// var b = a.slice(0, 2);
+// a.splice(1, 3);
+// var b = a.splice(3, 0, "ABC", "XYZ", "MNO")
+
+// // console.log(b);
+// console.log(a);
+
+// var a = [];
+// for (var i = 100; i > 0; i--){
+//     a.push("abc" + i);
+// }
+// console.log(a);
+
+var a = ["abc", "egf", "hij", "klm", "mno"];
+
+// var b = a.indexOf("egf");
+// console.log(b);
+
+for(var i = 0; i < a.length; i++){
+    console.log(a[i]);
+    if ("abc" == a[i]){
+        alert("word found");
+    }
+    break;
+}
